7eff3fa7eaee15ef710a7e22b7ea667bfeca32a6
Praytic <chernogorovvm@gmail.com>
2016-10-23 20:08:00 +0300

    Refactor Task3Test and create Task2Test

---
+++ src/test/kotlin/com/vchergnogorov/Task2Test.kt
@@ -0,0 +1,18 @@
+package com.vchergnogorov
+
+import com.vchernogorov.Task2
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Task2Test {
+
+    @Test
+    fun simpleTest() {
+        val task2 = Task2()
+        task2.task1.result = mutableMapOf(1.0 to 1.0, 2.0 to 4.0, 3.0 to 9.0)
+        task2.run()
+        val actual = task2.result.values.toTypedArray()
+        val expected = arrayOf(2.5, 6.5)
+        Assertions.assertArrayEquals(expected, actual)
+    }
+}
